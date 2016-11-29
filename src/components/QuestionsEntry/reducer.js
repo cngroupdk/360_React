f0de12d1e95b7eeb0932bb54156005d0ba53fc15
AndreyBotnarenko <andrey.botnarenko@gmail.com>
2016-11-29 14:17:13 +0100

    actions and reducer for level-entry page corrections

--- src/components/QuestionsEntry/reducer.js
+++ src/components/QuestionsEntry/reducer.js
@@ -2,16 +2,33 @@ import {handleActions} from 'redux-actions';
 import Immutable from 'immutable';
 
 import {
-    SUBMIT_ANSWERS,
+    REQUEST_QUESTIONS,
+    RECEIVE_QUESTIONS,
 } from './actions';
 
-const submitAssessment = handleActions({
-    [SUBMIT_ANSWERS]: (state) => {
+const levelEntry = handleActions({
+    [REQUEST_QUESTIONS]: (state) => {
         return state.withMutations(newState =>
             newState
                 .setIn(['isError'], false)
         );
     },
+
+    [RECEIVE_QUESTIONS]:{
+        next(state, action) {
+            return state.withMutations(newState => {
+                newState
+                    .setIn(['isError'], false)
+                    .setIn(['isCollegue'])
+            });
+        },
+        throw(state) {
+            return state.withMutations(newState =>
+                newState
+                    .setIn(['isError'], true)
+            );
+        },
+    }
 
 }, Immutable.fromJS({
     isLoaded: false,
--- src/pages/LevelEntryPage.js
+++ src/pages/LevelEntryPage.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 import { ContentContainer} from '../components/common/assets/styles/ContentContainer';
 import { ContentHeader} from '../components/common/assets/styles/ContentHeader';
 import { StyledButton } from '../components/common/assets/styles/StyledButton';
 
-export default class LevelEntryPage extends Component {
+import { fetchQuestions } from '../components/LevelEntry/actions';
+
+class LevelEntryPage extends Component {
+
+    componentDidMount() {
+        this.fetchAllData();
+    }
+
+    fetchAllData() {
+        this.props.fetchQuestions();
+    }
+
     render() {
         return (
-            <div>
-                <ContentContainer>
-                    <ContentHeader> LevelEntryPage </ContentHeader>
+            <ContentContainer>
+                <ContentHeader> Enter proficiency level </ContentHeader>
 
-                    <StyledButton> <Link to="/questions-entry">Proceed to questions</Link></StyledButton>
+                <button>Junior</button>
+                <button>Middle</button>
+                <button>Senior</button>
 
-                </ContentContainer>
-            </div>
+                <StyledButton> <Link to="/questions-entry">Proceed to questions</Link></StyledButton>
+
+            </ContentContainer>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const questions = state.get('peopleList');
+
+    return {
+        questions: questions.get('peopleList'),
+        isLoaded: questions.get('isLoaded'),
+        isError: questions.get('isError'),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    {fetchQuestions},
+)(LevelEntryPage);

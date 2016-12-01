1ef1589f9d35215eb1a6a14aec945e9aec0412ee
Oleksandra Tkachenko <solarua@gmail.com>
2016-12-01 10:53:09 +0100

    Dev otk (#10)
    react-radio-group added

--- src/pages/LevelPage.js
+++ src/pages/LevelPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { RadioGroup, Radio } from 'react-radio-group';
 
 import { ContentContainer} from '../components/common/assets/styles/ContentContainer';
 import { ContentHeader} from '../components/common/assets/styles/ContentHeader';
@@ -10,14 +11,17 @@ export default class LevelPage extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {level: '', personName: props.location.query.name };
+        this.state = {
+            level: '',
+            personName: props.location.query.name
+        };
         this.handleProfLevelChange = this.handleProfLevelChange.bind(this);
     }
 
 
-    handleProfLevelChange(e) {
+    handleProfLevelChange(value) {
         this.setState({
-            level: e.target.value
+            level: value
         });
     }
 
@@ -27,14 +31,16 @@ export default class LevelPage extends Component {
                 <ContentHeader> Please, choose the proficiency level for {this.state.personName}</ContentHeader>
 
                 <RadioWrapper>
-                    <input type="radio" name="prof-level" value="JUNIOR" onChange={this.handleProfLevelChange}/> Junior
-                    <input type="radio" name="prof-level" value="MIDDLE" onChange={this.handleProfLevelChange}/> Middle
-                    <input type="radio" name="prof-level" value="SENIOR" onChange={this.handleProfLevelChange}/> Senior
+                    <RadioGroup name="prof-level" onChange={this.handleProfLevelChange}>
+                        <label><Radio value="JUNIOR" /> Junior</label>
+                        <label><Radio value="MIDDLE" /> Middle</label>
+                        <label><Radio value="SENIOR" /> Senior</label>
+                    </RadioGroup>
                 </RadioWrapper>
 
-                    <StyledButton xyAlign disabled={this.state.level === '' ? true : false}>
-                        <Link className={this.state.level === '' ? 'disabled-link' : ''} to="/questions-entry">Proceed to questions</Link>
-                    </StyledButton>
+                <StyledButton xyAlign disabled={this.state.level === '' ? true : false}>
+                    <Link className={this.state.level === '' ? 'disabled-link' : ''} to="/questions-entry">Proceed to questions</Link>
+                </StyledButton>
 
             </ContentContainer>
         )

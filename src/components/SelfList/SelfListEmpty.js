872ec610763f738d72bb83721083c77ecc36fda4
Oleksandra Tkachenko <tkachenko@cngroup.dk>
2016-12-07 10:24:00 +0100

    Minor style changes on the Self and People Pages.

--- src/components/SelfList/SelfListEmpty.js
+++ src/components/SelfList/SelfListEmpty.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { TableRow, TableCell } from '../common/assets/styles/PersonRow';
+import { CenteredContent } from '../common/assets/styles/CenteredContent';
 
 export const SelfListEmpty = () => (
     <TableRow>
-        <TableCell fluid sm={12}>You don't have any self assessment for now</TableCell>
+        <TableCell fluid sm={12}>
+            <CenteredContent data-xyAlign>You don't have any self assessment for now</CenteredContent>
+        </TableCell>
     </TableRow>
 );
--- src/components/common/assets/styles/CenteredContent.js
+++ src/components/common/assets/styles/CenteredContent.js
@@ -4,5 +4,10 @@ import styled from 'styled-components';
 export const CenteredContent = styled.div`
     position: absolute;
     top: 50%;
-    transform: translateY(-50%); 
+    
+    ${ props => props[`data-xyAlign`] ?
+        'left: 50%; ' +
+        'transform: translate(-50%, -50%);'
+        :
+        'transform: translateY(-50%);' }
 `;

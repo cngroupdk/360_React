34180ad9a90fc9be071411b67d746aa49514c483
Andrey Botnarenko <andrey.botnarenko@gmail.com>
2016-12-14 12:10:40 +0100

    Dev andrey (#36)
    LevelPage and Reasonpage correct redirection and refactor

--- src/pages/ReasonPage.js
+++ src/pages/ReasonPage.js
@@ -1,49 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { apiPost } from '../api';
+import ReasonEntry from '../components/ReasonEntry/ReasonEntry'
+import { sendReason } from '../components/ReasonEntry/actions';
 
-import { ContentContainer} from '../components/common/assets/styles/ContentContainer';
-import { ContentHeader} from '../components/common/assets/styles/ContentHeader';
-import { StyledLink } from '../components/common/assets/styles/StyledLink';
-import { StyledTextArea } from '../components/common/assets/styles/StyledTextArea';
-
-export default class ReasonPage extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {reason: ''};
-        this._handleReasonEnter = this._handleReasonEnter.bind(this);
-    }
-
-    _handleReasonEnter(e) {
-        this.setState({
-            reason: e.target.value
-        });
-
-        apiPost.post('/assessments/reason', {
-            reason: e.target.value,
-            id: this.props.location.query.id
-        });
-    }
+class ReasonPage extends Component {
 
     render() {
+        const {
+            sendReason,
+            nextStep,
+        } = this.props;
+
         return (
-            <ContentContainer>
-                <ContentHeader>Warning! Person is not your co-worker.</ContentHeader>
-
-                <StyledTextArea autoFocus
-                                rows="4"
-                                cols="50"
-                                placeholder="Enter your reason here (min. 10 characters)..."
-                                onChange={this._handleReasonEnter}></StyledTextArea>
-
-                    <StyledLink disabled={this.state.reason.length < 10}
-                                to={{
-                                    pathname: "/level",
-                                    query: {id: this.props.location.query.id}
-                                }}>
-                        Proceed further</StyledLink>
-            </ContentContainer>
+            <ReasonEntry sendReason={sendReason} nextStep={nextStep} assessmentId={this.props.location.query.id}/>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const nextStep = state.get('reasonEntry');
+
+    return {
+        nextStep: nextStep.get('nextStep'),
+        isError: nextStep.get('isError'),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    {sendReason},
+)(ReasonPage);

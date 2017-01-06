0b43010b40d2b46f43e512f09f3aa07ec2671e74
AndreyBotnarenko <andrey.botnarenko@gmail.com>
2017-01-06 14:52:51 +0100

    validation bug fixed

--- src/components/ReasonLevelAssessmentWrapper/QuestionList/SkillsList.js
+++ src/components/ReasonLevelAssessmentWrapper/QuestionList/SkillsList.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import QuestionsList from './QuestionsList';
 
 export default class SkillsList extends Component {
+
+  componentDidMount() {
+    this.props.checkIfSubmittable()
+  }
+
   render() {
     const {
       assessment,
